27aedd7bd30ccfe1432f0d8aa4db148e09969b3f
Josh Wilson <joshua.wilson@vanderbilt.edu>
2020-10-21 15:09:53 -0400

    Transforms working, but Snowpack is not.

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MagicMirror from "./magic-mirror";
-import config from "../config/config";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MagicMirror from './magic-mirror';
+import config from '../config/config';
 
 // add the CSS to the page
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "roboto-fontface/css/roboto-condensed/roboto-condensed-fontface.css";
-import "./main.css";
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'roboto-fontface/css/roboto-condensed/roboto-condensed-fontface.css';
+import './main.css';
 
 // uncomment the next line if you want to use custom CSS
 /* import ../css/custom.css */
 
 // Get the DOM element for React to render into
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
 // Render MagicMirror with React
 ReactDOM.render(<MagicMirror initialConfig={config} />, root);
 
 // Automatically updates the screen when source files are changed.
 // Certain changes will still require you to quit and restart `mm start`.
+// #region hot reloading
 if (import.meta.hot) {
   import.meta.hot.accept();
   import.meta.hot.accept(['../config/config', './magic-mirror'], ({ deps }) => {
@@ -28,3 +29,4 @@ if (import.meta.hot) {
   });
   import.meta.hot.dispose(() => ReactDOM.unmountComponentAtNode(root));
 }
+// #endregion
--- src/magic-mirror.tsx
+++ src/magic-mirror.tsx
@@ -56,12 +56,12 @@ function MagicMirrorModule({
 function MagicMirror({ initialConfig }: { initialConfig: Config }) {
   // initialConfig is only initial arg, the component will copy it and manage the copy
   useInitializeConfig(() => initializeConfigClient(initialConfig));
-  const config = useCurrentConfig();
+  const modules = useCurrentConfig(c => c.modules);
 
   return (
     <React.StrictMode>
       <ModuleLayout>
-        {config.modules.map((m) => (
+        {modules.map((m) => (
           <MagicMirrorModule {...m} key={m.identifier} />
         ))}
       </ModuleLayout>
